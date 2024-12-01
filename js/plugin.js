8fb3b5e2be202e2477038b7ca8013f926d74f513
nkchocoai <nkai3359@gmail.com>
2024-12-01 16:33:21 +0900

    CSS and JS as external files

---
+++ js/plugin.js
@@ -0,0 +1,34 @@
+// Listen to plugin creation
+eagle.onPluginCreate(async (plugin) => {
+
+    // Get the current theme
+    const theme = await eagle.app.theme;
+    document.body.setAttribute('theme', theme);
+
+    // Get the selected item
+    const selected = await eagle.item.getSelected();
+    const item = selected[0];
+
+
+    const positivePrompt = document.getElementById("positive_prompt");
+
+    if (item.ext == "png") {
+        const sharp = require("sharp");
+
+        const metadata = await sharp(item.filePath).metadata();
+        const params = metadata.comments.find((comment) => comment.keyword == "parameters");
+        positivePrompt.innerText = params.text;
+    } else {
+        const ExifReader = require("exifreader");
+        const iconv = require("iconv-lite");
+
+        const tags = await ExifReader.load(item.filePath);
+        const exifString = iconv.decode(tags['UserComment'].value.slice(8), "utf-16");
+        positivePrompt.innerText = exifString;
+    }
+});
+
+// Listen to theme changes
+eagle.onThemeChanged((theme) => {
+    document.body.setAttribute('theme', theme);
+});
