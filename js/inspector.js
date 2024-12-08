ccf33af01c91026f6cc2334e3fa28e77d6884b39
nkchocoai <nkai3359@gmail.com>
2024-12-08 17:32:46 +0900

    Refactoring

--- js/inspector.js
+++ js/inspector.js
@@ -8,9 +8,9 @@ eagle.onPluginCreate(async (plugin) => {
     const selected = await eagle.item.getSelected();
     const item = selected[0];
 
-
     const positivePrompt = document.getElementById("metadata");
 
+    const { getParameters } = require(`${__dirname}/js/utils`);
     positivePrompt.innerText = await getParameters(item);
 });
 
@@ -18,20 +18,3 @@ eagle.onPluginCreate(async (plugin) => {
 eagle.onThemeChanged((theme) => {
     document.body.setAttribute('theme', theme);
 });
-
-async function getParameters(item) {
-    if (item.ext == "png") {
-        const sharp = require("sharp");
-
-        const metadata = await sharp(item.filePath).metadata();
-        const params = metadata.comments.find((comment) => comment.keyword == "parameters");
-        return params.text;
-    } else {
-        const ExifReader = require("exifreader");
-        const iconv = require("iconv-lite");
-
-        const tags = await ExifReader.load(item.filePath);
-        const exifString = iconv.decode(tags['UserComment'].value.slice(8), "utf-16");
-        return exifString;
-    }
-}
