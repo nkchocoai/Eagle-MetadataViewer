bda6a6896d1bde2207d6abcab0d6d0de438f709d
nkchocoai <nkai3359@gmail.com>
2024-12-09 00:14:26 +0900

    Show save result alert.

--- js/main.js
+++ js/main.js
@@ -10,23 +10,38 @@ eagle.onPluginCreate(async (plugin) => {
         const checkboxPositive = document.getElementById("checkbox-positive");
         const checkboxNegative = document.getElementById("checkbox-negative");
         const checkboxParameters = document.getElementById("checkbox-parameters");
-        for (let item of selected) {
-            const parameters = await getParameters(item);
-            const parsedParameters = parse(parameters);
-            let outputParameters = [];
-            if (checkboxPositive.checked) {
-                outputParameters.push(parsedParameters["Positive"]);
-            }
-            if (checkboxNegative.checked) {
-                outputParameters.push("Negative prompt:" + parsedParameters["Negative"]);
-            }
-            if (checkboxParameters.checked) {
-                outputParameters.push(parsedParameters["Settings"]);
-            }
 
-            item.annotation = outputParameters.join("\n");
-            await item.save();
+        let succeededItem = [];
+        let failedItem = [];
+
+        for (let item of selected) {
+            try {
+                const parameters = await getParameters(item);
+                const parsedParameters = parse(parameters);
+                let outputParameters = [];
+                if (checkboxPositive.checked) {
+                    outputParameters.push(parsedParameters["Positive"]);
+                }
+                if (checkboxNegative.checked) {
+                    outputParameters.push("Negative prompt:" + parsedParameters["Negative"]);
+                }
+                if (checkboxParameters.checked) {
+                    outputParameters.push(parsedParameters["Settings"]);
+                }
+
+                item.annotation = outputParameters.join("\n");
+                let result = await item.save();
+                if (result) {
+                    succeededItem.push(item);
+                } else {
+                    failedItem.push(item);
+                }
+            } catch (error) {
+                console.error(error);
+                failedItem.push(item);
+            }
         }
+        alertSaveResult(succeededItem, failedItem);
     });
 
     document.getElementById("send-to-tags").addEventListener("click", async function (e) {
@@ -34,27 +49,53 @@ eagle.onPluginCreate(async (plugin) => {
 
         const textareaReplace = document.getElementById("textarea-replace");
         const checkboxToLower = document.getElementById("checkbox-to-lower");
-        for (let item of selected) {
-            const parameters = await getParameters(item);
-            const parsedParameters = parse(parameters);
 
-            let tags = parseTags(parsedParameters["Positive"]);
-            let replacedTags = [];
-            for (let tag of tags) {
-                for (let line of textareaReplace.value.split(/\r\n|\n/)) {
-                    const [pattern, replacement] = line.split(",");
-                    tag = tag.replaceAll(pattern, replacement);
+        let succeededItems = [];
+        let failedItems = [];
+
+        for (let item of selected) {
+            try {
+                const parameters = await getParameters(item);
+                const parsedParameters = parse(parameters);
+
+                let tags = parseTags(parsedParameters["Positive"]);
+                let replacedTags = [];
+                for (let tag of tags) {
+                    for (let line of textareaReplace.value.split(/\r\n|\n/)) {
+                        const [pattern, replacement] = line.split(",");
+                        tag = tag.replaceAll(pattern, replacement);
+                    }
+                    if (checkboxToLower.checked) {
+                        tag = tag.toLowerCase();
+                    }
+                    replacedTags.push(tag);
                 }
-                if (checkboxToLower.checked) {
-                    tag = tag.toLowerCase();
+                item.tags = replacedTags;
+                let result = await item.save();
+                if (result) {
+                    succeededItems.push(item);
+                } else {
+                    failedItems.push(item);
                 }
-                replacedTags.push(tag);
+            } catch (error) {
+                console.error(error);
+                failedItems.push(item);
             }
-            item.tags = replacedTags;
-            await item.save();
         }
+        alertSaveResult(succeededItems, failedItems);
     });
 });
+
+function alertSaveResult(succeededItems, failedItems) {
+    let s = `${succeededItems.length} items saved tags.`;
+    if (failedItems.length > 0) {
+        s += "\n\n[Failed]";
+        for (let item of failedItems) {
+            s += `\n- ${item.name}.${item.ext}`
+        }
+    }
+    alert(s);
+}
 
 // Listen to theme changes
 eagle.onThemeChanged((theme) => {
